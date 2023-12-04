ceb683c68396b3aa36901448064f45f326c14df7
IshantKr632 <138979677+IshantKr632@users.noreply.github.com>
2023-12-04 07:14:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed('.text-decorated', {
+    strings: ['Website Developing', 'Graphic Deisgning', 'UI/UX Designing'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
